5d104bff59f2b2eb2a2e17991f5f9c8eeda481fc
Pavel Novikov <spl.novikov@gmail.com>
2019-07-17 19:00:41 -0500

    updated albums api - added lang

--- src/api/albums.js
+++ src/api/albums.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import apiRoutes from '../constants/apiRoutes';
 
-const getAlbums = () => axios.get(`${apiRoutes.albums}`);
-// todo: lang by intl
+import getLanguage from '../utils/localization';
+
+const { language } = getLanguage();
+
+const getAlbums = () =>
+  axios.get(`${apiRoutes.albums}`, { params: { lang: language } });
+
 const getAlbum = id =>
-  axios.get(`${apiRoutes.albums}/${id}`, { params: { lang: 'rus' } });
+  axios.get(`${apiRoutes.albums}/${id}`, { params: { lang: language } });
 
 export default { getAlbums, getAlbum };
