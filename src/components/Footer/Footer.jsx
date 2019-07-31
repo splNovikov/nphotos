7e14258d7dd1a7d6b1e84e2e8e9f56b104e35153
Pavel Novikov <pnovikov@pier1.com>
2019-07-30 19:27:54 -0500

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,75 +1,17 @@
 import React from 'react';
-import {
-  Segment,
-  Container,
-  Grid,
-  List,
-  Header,
-  Image,
-  Divider
-} from 'semantic-ui-react';
+import { Segment, Container, List, Image } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.svg';
+import './Footer.scss';
 
 const Footer = () => (
-  <Segment
-    secondary
-    vertical
-    color="orange"
-    style={{ margin: '5em 0em 0em', padding: '2em 0' }}
-  >
+  <Segment secondary vertical className="footer">
     <Container textAlign="center">
-      <Grid divided stackable>
-        <Grid.Column width={3}>
-          <Header as="h4" content="Group 1" />
-          <List link>
-            <List.Item as="a">Link One</List.Item>
-            <List.Item as="a">Link Two</List.Item>
-            <List.Item as="a">Link Three</List.Item>
-            <List.Item as="a">Link Four</List.Item>
-          </List>
-        </Grid.Column>
-        <Grid.Column width={3}>
-          <Header as="h4" content="Group 2" />
-          <List link>
-            <List.Item as="a">Link One</List.Item>
-            <List.Item as="a">Link Two</List.Item>
-            <List.Item as="a">Link Three</List.Item>
-            <List.Item as="a">Link Four</List.Item>
-          </List>
-        </Grid.Column>
-        <Grid.Column width={3}>
-          <Header as="h4" content="Group 3" />
-          <List link>
-            <List.Item as="a">Link One</List.Item>
-            <List.Item as="a">Link Two</List.Item>
-            <List.Item as="a">Link Three</List.Item>
-            <List.Item as="a">Link Four</List.Item>
-          </List>
-        </Grid.Column>
-        <Grid.Column width={7}>
-          <Header as="h4" content="Footer Header" />
-          <p>
-            Extra space for a call to action inside the footer that could help
-            re-engage users.
-          </p>
-        </Grid.Column>
-      </Grid>
-
-      <Divider section />
-      <Image centered size="small" src={logo} />
+      <Image centered size="tiny" src={logo} />
       <List horizontal divided link size="small">
-        <List.Item as="a" href="#">
-          Site Map
-        </List.Item>
-        <List.Item as="a" href="#">
+        <List.Item as={NavLink} to="/">
           Contact Us
-        </List.Item>
-        <List.Item as="a" href="#">
-          Terms and Conditions
-        </List.Item>
-        <List.Item as="a" href="#">
-          Privacy Policy
         </List.Item>
       </List>
     </Container>
--- src/views/About/AboutView.jsx
+++ src/views/About/AboutView.jsx
@@ -6,48 +6,11 @@ import {
   Grid,
   Header,
   Image,
-  List,
   Segment
 } from 'semantic-ui-react';
 
 const AboutView = () => (
-  <div>
-    <Segment vertical>
-      <Grid container stackable verticalAlign="middle">
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never
-              thought possible. Let us delight your customers and empower your
-              needs... through pure data analytics.
-            </p>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes that is right, you thought it was the stuff of dreams, but
-              even bananas can be bioengineered.
-            </p>
-          </Grid.Column>
-          <Grid.Column floated="right" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="/images/wireframe/white-image.png"
-            />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign="center">
-            <Button size="huge">Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+  <React.Fragment>
     <Segment style={{ padding: '0em' }} vertical>
       <Grid celled="internally" columns="equal" stackable>
         <Grid.Row textAlign="center">
@@ -106,41 +69,6 @@ const AboutView = () => (
         </Button>
       </Container>
     </Segment>
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="About" />
-              <List link inverted>
-                <List.Item as="a">Sitemap</List.Item>
-                <List.Item as="a">Contact Us</List.Item>
-                <List.Item as="a">Religious Ceremonies</List.Item>
-                <List.Item as="a">Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Services" />
-              <List link inverted>
-                <List.Item as="a">Banana Pre-Order</List.Item>
-                <List.Item as="a">DNA FAQ</List.Item>
-                <List.Item as="a">How To Access</List.Item>
-                <List.Item as="a">Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4" inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could
-                help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
-  </div>
+  </React.Fragment>
 );
 export default AboutView;
