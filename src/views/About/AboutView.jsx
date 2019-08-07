42632f6c0d6a98a182507a934bf36fb35db64461
Pavel Novikov <pnovikov@pier1.com>
2019-08-07 17:22:32 -0500

    updated about-view

--- src/views/About/AboutView.jsx
+++ src/views/About/AboutView.jsx
@@ -1,74 +1,21 @@
 import React from 'react';
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Image,
-  Segment
-} from 'semantic-ui-react';
+import { Grid, Image, Segment } from 'semantic-ui-react';
+
+const isFetching = false;
 
 const AboutView = () => (
-  <React.Fragment>
-    <Segment style={{ padding: '0em' }} vertical>
-      <Grid celled="internally" columns="equal" stackable>
-        <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              What a Company
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              That is what they all say about us
-            </p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              I shouldnt have gone with their competitor.
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as="h3" style={{ fontSize: '2em' }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned
-          how to master the art of doing nothing by providing massive amounts of
-          whitespace and generic content that can seem massive, monolithic and
-          worth your attention.
-        </p>
-        <Button as="a" size="large">
-          Read More
-        </Button>
-        <Divider
-          as="h4"
-          className="header"
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-        >
-          <a href="#any">Case Studies</a>
-        </Divider>
-        <Header as="h3" style={{ fontSize: '2em' }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur
-          filler content, but it is really true. It took years of gene splicing
-          and combinatory DNA research, but our bananas can really dance.
-        </p>
-        <Button as="a" size="large">
-          I am Still Quite Interested
-        </Button>
-      </Container>
-    </Segment>
-  </React.Fragment>
+  <Segment className="about-view no-borders" loading={isFetching}>
+    <Grid container stackable>
+      <Grid.Column width={8}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+      </Grid.Column>
+    </Grid>
+  </Segment>
 );
 export default AboutView;
