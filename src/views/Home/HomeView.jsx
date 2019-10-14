e6beffb5c2fca29e2693a2331f58154929b4d8cc
Pavel Novikov <pnovikov@pier1.com>
2019-10-14 18:37:04 +0400

    added validation for root component rerendering

--- src/views/Home/HomeView.jsx
+++ src/views/Home/HomeView.jsx
@@ -6,6 +6,19 @@ import PrefilledBanner from './components/PrefilledBanner';
 
 @observer
 class HomeView extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    Object.entries(this.props).forEach(
+      ([key, val]) =>
+        prevProps[key] !== val && console.log(`Prop '${key}' changed`)
+    );
+    if (this.state) {
+      Object.entries(this.state).forEach(
+        ([key, val]) =>
+          prevState[key] !== val && console.log(`State '${key}' changed`)
+      );
+    }
+  }
+
   render() {
     return (
       <div className="home-view">
