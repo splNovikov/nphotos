1f9d80bc2a1c480ff327a617a35441d506f6b263
Pavel Novikov <pnovikov@pier1.com>
2019-10-14 19:01:56 +0400

    added observable footer navigation - to fix always active link in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Segment, Container, List, Image } from 'semantic-ui-react';
+import { Segment, Container, Image } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
-import { injectIntl, intlShape } from 'react-intl';
 import { observer } from 'mobx-react';
 
-import appRoutes from '../../constants/appRoutes';
 import logo from '../../assets/images/logo.svg';
 import SocialLinks from '../SocialLinks';
+import FooterNavigation from '../FooterNavigation';
 
 import './Footer.scss';
 
-const Footer = ({ intl: { formatMessage } }) => (
+const Footer = () => (
   <Segment raised vertical className="footer">
     <Container textAlign="center">
       <div className="links-wrapper">
@@ -26,57 +25,9 @@ const Footer = ({ intl: { formatMessage } }) => (
         to="/"
       />
 
-      <List horizontal divided link size="small">
-        <List.Item
-          exact
-          as={NavLink}
-          to="/"
-          content={formatMessage({
-            id: 'navigationMenu.home',
-            defaultMessage: 'home'
-          })}
-        />
-        <List.Item
-          exact
-          as={NavLink}
-          to={appRoutes.albums}
-          content={formatMessage({
-            id: 'navigationMenu.albums',
-            defaultMessage: 'albums'
-          })}
-        />
-        <List.Item
-          as={NavLink}
-          to={appRoutes.about}
-          content={formatMessage({
-            id: 'navigationMenu.about',
-            defaultMessage: 'about'
-          })}
-        />
-        <List.Item
-          as={NavLink}
-          to={appRoutes.contacts}
-          content={formatMessage({
-            id: 'navigationMenu.contacts',
-            defaultMessage: 'contacts'
-          })}
-        />
-        <List.Item
-          as={NavLink}
-          to={appRoutes.priceList}
-          content={formatMessage({
-            id: 'navigationMenu.priceList',
-            defaultMessage: 'price list'
-          })}
-        />
-      </List>
+      <FooterNavigation />
     </Container>
   </Segment>
 );
 
-Footer.propTypes = {
-  // eslint-disable-next-line react/require-default-props
-  intl: intlShape
-};
-
-export default observer(injectIntl(Footer));
+export default observer(Footer);
---
+++ src/components/FooterNavigation/FooterNavigation.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { List } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+import { injectIntl, intlShape } from 'react-intl';
+import { inject, observer } from 'mobx-react';
+
+import appRoutes from '../../constants/appRoutes';
+
+import './FooterNavigation.scss';
+
+@inject(({ routingStore }) => ({
+  // added this to observe location - for state changing
+  location: routingStore.location
+}))
+@observer
+class Footer extends Component {
+  static propTypes = {
+    // eslint-disable-next-line react/require-default-props
+    intl: intlShape
+  };
+
+  render() {
+    const {
+      intl: { formatMessage }
+    } = this.props;
+
+    return (
+      <List horizontal divided link size="small" className="footer-navigation">
+        <List.Item
+          as={NavLink}
+          exact
+          to="/"
+          content={formatMessage({
+            id: 'navigationMenu.home',
+            defaultMessage: 'home'
+          })}
+        />
+        <List.Item
+          as={NavLink}
+          exact
+          to={appRoutes.albums}
+          content={formatMessage({
+            id: 'navigationMenu.albums',
+            defaultMessage: 'albums'
+          })}
+        />
+        <List.Item
+          as={NavLink}
+          to={appRoutes.about}
+          content={formatMessage({
+            id: 'navigationMenu.about',
+            defaultMessage: 'about'
+          })}
+        />
+        <List.Item
+          as={NavLink}
+          to={appRoutes.contacts}
+          content={formatMessage({
+            id: 'navigationMenu.contacts',
+            defaultMessage: 'contacts'
+          })}
+        />
+        <List.Item
+          as={NavLink}
+          to={appRoutes.priceList}
+          content={formatMessage({
+            id: 'navigationMenu.priceList',
+            defaultMessage: 'price list'
+          })}
+        />
+      </List>
+    );
+  }
+}
+
+export default injectIntl(Footer);
