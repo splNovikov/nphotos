96c241a63ed21ac554abe1bfb9bedcc6d124d6e5
Pavel Novikov <pnovikov@pier1.com>
2019-10-15 13:57:32 +0400

    added memoization to getLanguageMessages function

--- src/utils/localization.js
+++ src/utils/localization.js
@@ -1,6 +1,15 @@
 import locales from '../locales';
 
 const getLanguageMessages = () => {
+  const { memoLang, memoMess } = getLanguageMessages;
+
+  if (memoLang && memoMess) {
+    return {
+      language: memoLang,
+      messages: memoMess
+    };
+  }
+
   // Define user's language. Different browsers have the user locale defined
   // on different fields on the `navigator` object, so we make sure to account
   // for these different by checking all of them
@@ -13,6 +22,9 @@ const getLanguageMessages = () => {
   // Try full locale, try locale without region code, fallback to 'en'
   const messages =
     locales[languageWithoutRegionCode] || locales[language] || locales.en;
+
+  getLanguageMessages.memoLang = language;
+  getLanguageMessages.memoMess = messages;
 
   return { language, messages };
 };
