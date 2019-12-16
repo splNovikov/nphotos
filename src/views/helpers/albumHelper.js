fe1549c6c3941a2cfadc8e1d382e6b8e4082b44c
Pavel Novikov <spl.novikov@gmail.com>
2019-12-16 14:55:04 +0400

    one more trivial fix applied

--- src/views/helpers/albumHelper.js
+++ src/views/helpers/albumHelper.js
@@ -1,20 +1,22 @@
 const hasImages = album => album && album.images && album.images.length;
 
 const mapToGridEntity = album =>
-  album.images.reduce(
-    (acc, i) =>
-      i.id
-        ? [
-            ...acc,
-            {
-              ...i,
-              cover: i.previewSrc,
-              description: i.title,
-              title: undefined
-            }
-          ]
-        : acc,
-    []
-  );
+  album && album.images && album.images.length
+    ? album.images.reduce(
+        (acc, i) =>
+          i.id
+            ? [
+                ...acc,
+                {
+                  ...i,
+                  cover: i.previewSrc,
+                  description: i.title,
+                  title: undefined
+                }
+              ]
+            : acc,
+        []
+      )
+    : [];
 
 export default { hasImages, mapToGridEntity };
