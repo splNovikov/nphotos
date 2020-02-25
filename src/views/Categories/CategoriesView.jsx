71d4837979053bc441e0467ed3b6c6c1021d5e64
Pavel Novikov <spl.novikov@gmail.com>
2020-02-25 20:37:57 +0400

    Added Add Block to Categories page

--- src/views/Categories/CategoriesView.jsx
+++ src/views/Categories/CategoriesView.jsx
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import { inject, observer } from 'mobx-react';
+import { Button, Segment } from 'semantic-ui-react';
+import { injectIntl } from 'react-intl';
 
 import Categories from '../../components/Categories';
+import userPermissions from '../../constants/userPermissions';
 
+import './CategoriesView.scss';
+
+@inject(({ userStore }) => ({
+  user: userStore.user
+}))
 @observer
 class CategoriesView extends Component {
   render() {
+    const {
+      intl: { formatMessage },
+      user: { permissions }
+    } = this.props;
+
     return (
       <div className="categories-view">
+        {permissions[userPermissions.canEditCategory] ? (
+          <div className="edit-segment-wrapper">
+            <Segment textAlign="right">
+              <Button
+                onClick={this.handleClickEdit}
+                labelPosition="left"
+                positive
+                icon="add"
+                content={formatMessage({
+                  id: 'common.add',
+                  defaultMessage: 'add'
+                })}
+              />
+            </Segment>
+          </div>
+        ) : null}
         <Categories />
       </div>
     );
   }
 }
 
-export default CategoriesView;
+CategoriesView.wrappedComponent.propTypes = {
+  intl: PropTypes.shape().isRequired,
+  user: PropTypes.shape({
+    permissions: PropTypes.shape({
+      [userPermissions.canEditCategory]: PropTypes.bool
+    })
+  }).isRequired
+};
+
+export default injectIntl(CategoriesView);
