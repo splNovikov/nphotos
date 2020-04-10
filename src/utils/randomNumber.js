2798a8472eaa3226ae5303bec5e3b6d23c611b0d
Pavel Novikov <spl.novikov@gmail.com>
2020-04-10 10:07:42 +0400

    fixed bug with duplicated banner

--- src/utils/randomNumber.js
+++ src/utils/randomNumber.js
@@ -1,11 +1,14 @@
 const randomNumber = (n, min, max, res = []) => {
+  if (n > max - min) {
+    return [];
+  }
   if (res.length === n) {
     return res;
   }
 
   const num = Math.floor(Math.random() * max) + min;
 
-  if (res.find(r => r === num)) {
+  if (res.filter(r => r === num).length) {
     return randomNumber(n, min, max, res);
   }
 
