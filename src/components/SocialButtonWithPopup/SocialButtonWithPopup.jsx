eae2a9fbb172bcb9d09ec1ddf9f52297993e42b8
Pavel Novikov <spl.novikov@gmail.com>
2020-09-01 19:07:02 +0400

    Updated popups content

--- src/components/SocialButtonWithPopup/SocialButtonWithPopup.jsx
+++ src/components/SocialButtonWithPopup/SocialButtonWithPopup.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import { Popup } from 'semantic-ui-react';
+import { injectIntl } from 'react-intl';
 
 import SocialButton from '../SocialButton';
 
-const SocialButtonWithPopup = ({ href, icon }) => (
+const SocialButtonWithPopup = ({ href, icon, intl: { formatMessage } }) => (
   <Popup
     position="bottom right"
-    content={href}
+    content={`${formatMessage({
+      id: 'socialButton.routeTo',
+      defaultMessage: 'route to'
+    })} ${icon}`}
     trigger={<SocialButton href={href} icon={icon} />}
   />
 );
 SocialButtonWithPopup.propTypes = {
+  intl: PropTypes.shape().isRequired,
   href: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired
 };
 
-export default observer(SocialButtonWithPopup);
+export default observer(injectIntl(SocialButtonWithPopup));
