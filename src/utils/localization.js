f3a71b0e4a7d735daba82a69233e5736db185c99
Pavel Novikov <spl.novikov@gmail.com>
2020-09-01 16:44:00 +0400

    temporary disabled localizations. switched to Russian only

--- src/utils/localization.js
+++ src/utils/localization.js
@@ -13,15 +13,18 @@ const getLanguageMessages = () => {
   // Define user's language. Different browsers have the user locale defined
   // on different fields on the `navigator` object, so we make sure to account
   // for these different by checking all of them
-  const language =
-    (navigator.languages && navigator.languages[0]) || navigator.language;
+  //   returns something like: en-US
+  // const language =
+  //   (navigator.languages && navigator.languages[0]) || navigator.language;
+  // todo: return back localization somehow. Because it is not working correctly now
+  const language = 'ru-Ru';
 
   // Split locales with a region code
   const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 
-  // Try full locale, try locale without region code, fallback to 'en'
+  // Try full locale, try locale without region code, fallback to 'ru'
   const messages =
-    locales[languageWithoutRegionCode] || locales[language] || locales.en;
+    locales[languageWithoutRegionCode] || locales[language] || locales.ru;
 
   getLanguageMessages.memoLang = languageWithoutRegionCode;
   getLanguageMessages.memoMess = messages;
