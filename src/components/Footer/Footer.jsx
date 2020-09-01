1b72f4704a70e4fd44782173943474ee1889dcaf
Pavel Novikov <spl.novikov@gmail.com>
2020-09-01 11:32:41 +0400

    added quality message to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Segment, Container, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Segment, Container, Image, Header } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { observer } from 'mobx-react';
+import { injectIntl } from 'react-intl';
 
 import logo from '../../assets/images/logo.svg';
 import SocialLinks from '../SocialLinks';
@@ -9,7 +11,7 @@ import FooterNavigation from '../FooterNavigation';
 
 import './Footer.scss';
 
-const Footer = () => (
+const Footer = ({ intl: { formatMessage } }) => (
   <Segment raised vertical className="footer">
     <Container textAlign="center">
       <div className="links-wrapper">
@@ -25,9 +27,20 @@ const Footer = () => (
         to="/"
       />
 
+      <Header as="h6">
+        {formatMessage({
+          id: 'footer.imageQualityAttention',
+          defaultMessage: 'upload'
+        })}
+      </Header>
+
       <FooterNavigation />
     </Container>
   </Segment>
 );
 
-export default observer(Footer);
+Footer.propTypes = {
+  intl: PropTypes.shape().isRequired
+};
+
+export default observer(injectIntl(Footer));
